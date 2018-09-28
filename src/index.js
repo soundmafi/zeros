a4cc4e706ac4aa71113e25136c4f488030040c3c
soundmafi <43148286+soundmafi@users.noreply.github.com>
2018-09-28 12:43:59 +0400

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zeros = 0;
+
+  for (var i = 1; i <= number; i++) {
+      if (i % 5 === 0 || i % 10 === 0) {
+          var currNum = i;
+          while (currNum % 10 === 0) {
+              currNum /= 10;
+              zeros++;
+          }
+
+          while (currNum % 5 === 0) {
+              currNum /= 5;
+              zeros++;
+          }
+      }
+  }
+
+  return zeros; 
+  
 }
